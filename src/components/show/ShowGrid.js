480b0bd4501843703e9c2b05449882bb594cd7a5
ravibatchala <batchalaraviteja@gmail.com>
2021-07-12 16:59:13 +0530

    Finished the starred page with custome hooks

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -3,19 +3,34 @@ import ShowCard from './ShowCard';
 import { FlexGrid } from '../styled';
 
 import IMG_NOT_FOUNT from '../../images/not-found.png';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMG_NOT_FOUNT}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMG_NOT_FOUNT}
+            summary={show.summary}
+            isStarred={isStarred}
+            onStarClick={onStarClick}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
